e62875a5d0d71c60c60e96603835242b7e67c323
m-stevenson <mitchell.stevenson@georgebrown.ca>
2026-05-04 16:52:50 -0400

    Update input styling

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -8,7 +8,7 @@ export function Input({
   return (
     <input
       className={cn(
-        "flex h-10 w-full rounded-md border border-gray-300 bg-white px-3 py-2 text-sm outline-none",
+        "rounded-md p-2 text-sm text-foreground border-2 border-border focus:border-border-strong focus:outline-none",
         className
       )}
       {...props}
